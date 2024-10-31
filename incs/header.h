0578071dbef583d9267e84c0a2df8b4a5d3686df
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-10-31 12:32:51 -0300

    añadiendo la estructura del grafo

---
+++ incs/header.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_DOCS 100
+
+typedef struct Nodo
+{
+    int id_doc;        // indice del documento
+    struct Nodo *next; // siguiente nodo
+} Nodo;
+
+typedef struct Grafo
+{
+    int numero_docs;             // numero de documentos del grafo
+    Nodo *lista_nodos[MAX_DOCS]; // arreglo con la lista
+
+} Grafo;
