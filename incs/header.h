0c288bea91fa8ef8bc3f18b7632fb1bbf927c5d2
mihel1 <miguelmaripillan0@gmail.com>
2024-11-04 15:36:25 -0300

    Actualice el header.
    Agregue librerías, los prototipos de funciones y actualice los struct.

--- incs/header.h
+++ incs/header.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <dirent.h>
+#include <ctype.h>
 
 #define MAX_DOCS 100
 
@@ -11,12 +14,20 @@ typedef struct Nodo
 } Nodo;
 
 typedef struct Grafo
-{
-    int numero_docs;             // numero de documentos del grafo
-    Nodo *lista_nodos[MAX_DOCS]; // arreglo con la lista
-
+{    /* Enlaces del grafo dirigido, entrada y salida para pagerank e indice invertido*/
+    Nodo *lista_adyacencia_salida[MAX_DOCS];     
+    Nodo *lista_adyacencia_entrada[MAX_DOCS];        
 } Grafo;
 
-void inicializar_grafo(Grafo *grafo);
+/* Variables globales*/
+extern MapeoDocumento mapeo_docs[MAX_DOCS];
+extern int total_docs
 
-void agregar_documento(Grafo *grafo, int id_doc);
+void inicializar_grafo(Grafo *grafo);
+void agregar_arista(Grafo *grafo, int origen, int destino);
+void construir_grafo(Grafo *grafo);
+void liberar_grafo(Grafo *grafo);
+int contar_enlaces_salida(Grafo *grafo, int id_doc);
+int contar_enlaces_entrada(Grafo *grafo, int id_doc);
+int obtener_id_doc(char *nombre_archivo);
+bool es_nombre_documento(char *nombre_archivo);
