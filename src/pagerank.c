49751b66af5dd43df3046c04d09c866a621bfd8d
Miguel-- <miguelmaripillan0@gmail.com>
2024-11-08 01:38:31 -0300

    Añadí sistema de linkeos.

--- src/pagerank.c
+++ src/pagerank.c
@@ -1,8 +1,7 @@
-#include "header.h"
+#include "../incs/header.h"
 
 void generate_text_files(int num_webs)
 {
-    // El número de archivos web debe ser mayor a 0 para poder pasar de este punto.
     if (num_webs <= 0)
     {
         fprintf(stderr, "El número de archivos web a generar debe ser MAYOR a 0.\n");
@@ -12,21 +11,20 @@ void generate_text_files(int num_webs)
     for (int i = 1; i <= num_webs; i++)
     {
         char web_name[MAX_NAME_WEB];
-        snprintf(web_name, sizeof(web_name), "web%d.txt", i); // Crear un nombre de archivo web.
+        snprintf(web_name, sizeof(web_name), "doc%d.txt", i);
 
-        // Crear un archivo web, en caso de no poder abrir el archivo sale un error.
         FILE *web = fopen(web_name, "w");
         if (web == NULL)
         {
-            fprintf(stderr, "Error al abrir el archivo web.");
+            fprintf(stderr, "Error al abrir el archivo web.\n");
             exit(EXIT_FAILURE);
         }
 
-        generate_random_text(web, web_name);
+        generate_random_text(web, web_name, num_webs, i);
     }
 }
 
-void generate_random_text(FILE *web, const char *web_name)
+void generate_random_text(FILE *web, const char *web_name, int num_docs, int current_doc)
 {
     // Generar texto aleatorio dentro de cada archivo web.
     for (int i = 0; i < MAX_CHARACTERS_WEB; i++)
@@ -36,6 +34,17 @@ void generate_random_text(FILE *web, const char *web_name)
         if (i < MAX_CHARACTERS_WEB - 1)
         {
             fprintf(web, " ");
+        }
+    }
+
+    /* Incluir aleatoriamente enlaces a otros documentos*/
+    int num_links = rand() % 3; /* Hasta 2 enlaces*/
+    for (int i = 0; i < num_links; i++)
+    {
+        int link_doc = rand() % num_docs + 1; /* Documento aleatorio de 1 a num_docs*/
+        if (link_doc != current_doc) /* No enlazar a sí mismo */
+        {
+            fprintf(web, "\nlink: doc%d", link_doc);
         }
     }
 
