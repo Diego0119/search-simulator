4f0b02f0de468d7263928d80d5d32ad8b4f5cc03
EhMigueh <miloaiza@umag.cl>
2024-11-18 23:23:28 -0300

    último commit

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -6,7 +6,7 @@ var searchData=
   ['de_20sistema_20de_20recuperación_20de_20información_3',['Simulador de Sistema de Recuperación de Información',['../index.html',1,'']]],
   ['de_20vista_20adicional_4',['Opción de vista adicional',['../index.html#autotoc_md6',1,'']]],
   ['del_20programa_5',['Uso del Programa',['../index.html#autotoc_md3',1,'']]],
-  ['del_20programa_6',['Salida del programa',['../index.html#autotoc_md7',1,'']]],
+  ['del_20programa_6',['Salida del programa',['../index.html#autotoc_md8',1,'']]],
   ['del_20proyecto_7',['Compilación del proyecto',['../index.html#autotoc_md4',1,'']]],
   ['display_5fpagerank_8',['display_pagerank',['../pagerank_8c.html#af774e2c127d30c57b1033c2874f056c3',1,'display_pagerank(Graph *graph, double *pagerank):&#160;pagerank.c'],['../pagerank_8h.html#ac1a384ff1bb89d7d63bb5e38a39f9ed6',1,'display_pagerank(Graph *, double *):&#160;pagerank.c']]],
   ['doc_2ec_9',['doc.c',['../doc_8c.html',1,'']]],
--- docs/html/search/all_a.js
+++ docs/html/search/all_a.js
@@ -5,6 +5,6 @@ var searchData=
   ['print_5finverted_5findex_2',['print_inverted_index',['../inverted__index_8c.html#a0dd6888286a2f08c29ef53e02f40fde9',1,'print_inverted_index(InvertedIndex **index):&#160;inverted_index.c'],['../inverted__index_8h.html#a8b68e91fdb924af1316a3c01948d9ccf',1,'print_inverted_index(InvertedIndex **):&#160;inverted_index.c']]],
   ['print_5fsearch_5fword_3',['print_search_word',['../inverted__index_8c.html#ab994f81c8baf9e5796c8a9ce761ae1ef',1,'print_search_word(InvertedIndex **index, char *word_to_search):&#160;inverted_index.c'],['../inverted__index_8h.html#a3e5c5ea1c346f40d0d7f524dc2c4a691',1,'print_search_word(InvertedIndex **, char *):&#160;inverted_index.c']]],
   ['programa_4',['Uso del Programa',['../index.html#autotoc_md3',1,'']]],
-  ['programa_5',['Salida del programa',['../index.html#autotoc_md7',1,'']]],
+  ['programa_5',['Salida del programa',['../index.html#autotoc_md8',1,'']]],
   ['proyecto_6',['Compilación del proyecto',['../index.html#autotoc_md4',1,'']]]
 ];
