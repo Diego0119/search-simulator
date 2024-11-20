553962340715e246076b642b3a57b7ba95f9ee23
EhMigueh <miloaiza@umag.cl>
2024-11-19 23:18:19 -0300

    Actualizando documentación

--- docs/html/search/all_a.js
+++ docs/html/search/all_a.js
@@ -3,7 +3,7 @@ var searchData=
   ['pagerank_2ec_0',['pagerank.c',['../pagerank_8c.html',1,'']]],
   ['pagerank_2eh_1',['pagerank.h',['../pagerank_8h.html',1,'']]],
   ['print_5finverted_5findex_2',['print_inverted_index',['../inverted__index_8c.html#a0dd6888286a2f08c29ef53e02f40fde9',1,'print_inverted_index(InvertedIndex **index):&#160;inverted_index.c'],['../inverted__index_8h.html#a8b68e91fdb924af1316a3c01948d9ccf',1,'print_inverted_index(InvertedIndex **):&#160;inverted_index.c']]],
-  ['print_5fsearch_5fword_3',['print_search_word',['../inverted__index_8c.html#ab994f81c8baf9e5796c8a9ce761ae1ef',1,'print_search_word(InvertedIndex **index, char *word_to_search):&#160;inverted_index.c'],['../inverted__index_8h.html#a3e5c5ea1c346f40d0d7f524dc2c4a691',1,'print_search_word(InvertedIndex **, char *):&#160;inverted_index.c']]],
+  ['print_5fsearch_5fword_5fwith_5fpagerank_3',['print_search_word_with_pagerank',['../inverted__index_8c.html#aed64c2ea34cfe6b9b40f4e164b81e8fb',1,'print_search_word_with_pagerank(InvertedIndex **index, char *word_to_search, Graph *graph, double *pagerank):&#160;inverted_index.c'],['../inverted__index_8h.html#a3364e51ef43ec033dec5d6c8854a0889',1,'print_search_word_with_pagerank(InvertedIndex **, char *, Graph *, double *):&#160;inverted_index.c']]],
   ['programa_4',['Uso del Programa',['../index.html#autotoc_md3',1,'']]],
   ['programa_5',['Salida del programa',['../index.html#autotoc_md8',1,'']]],
   ['proyecto_6',['Compilación del proyecto',['../index.html#autotoc_md4',1,'']]]
--- docs/html/search/functions_8.js
+++ docs/html/search/functions_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['print_5finverted_5findex_0',['print_inverted_index',['../inverted__index_8c.html#a0dd6888286a2f08c29ef53e02f40fde9',1,'print_inverted_index(InvertedIndex **index):&#160;inverted_index.c'],['../inverted__index_8h.html#a8b68e91fdb924af1316a3c01948d9ccf',1,'print_inverted_index(InvertedIndex **):&#160;inverted_index.c']]],
-  ['print_5fsearch_5fword_1',['print_search_word',['../inverted__index_8c.html#ab994f81c8baf9e5796c8a9ce761ae1ef',1,'print_search_word(InvertedIndex **index, char *word_to_search):&#160;inverted_index.c'],['../inverted__index_8h.html#a3e5c5ea1c346f40d0d7f524dc2c4a691',1,'print_search_word(InvertedIndex **, char *):&#160;inverted_index.c']]]
+  ['print_5fsearch_5fword_5fwith_5fpagerank_1',['print_search_word_with_pagerank',['../inverted__index_8c.html#aed64c2ea34cfe6b9b40f4e164b81e8fb',1,'print_search_word_with_pagerank(InvertedIndex **index, char *word_to_search, Graph *graph, double *pagerank):&#160;inverted_index.c'],['../inverted__index_8h.html#a3364e51ef43ec033dec5d6c8854a0889',1,'print_search_word_with_pagerank(InvertedIndex **, char *, Graph *, double *):&#160;inverted_index.c']]]
 ];
